35b6c01fe00028b7ce8e0c922b3f5ee3b94cd720
mstaczek <matemmatem2000@gmail.com>
2023-05-08 21:44:51 +0200

    clean repo

--- upload_blob.py
+++ upload_blob.py
@@ -1,6 +1,4 @@
 from google.cloud import storage
-import io
-import matplotlib.pyplot as plt
 
 
 def upload_blob(bucket_name, source_file_name, destination_blob_name):
@@ -17,32 +15,3 @@ def upload_blob(bucket_name, source_file_name, destination_blob_name):
     print(
         f"File {source_file_name} uploaded to {destination_blob_name}."
     )
-
-
-
-def upload_blob_old(bucket_name, image, destination_blob_name):
-    """Uploads a file to the bucket."""
-    # The ID of your GCS bucket
-    # bucket_name = "your-bucket-name"
-    #
-    # Image
-    # Image object 
-    #
-    # The ID of your GCS object
-    # destination_blob_name = "storage-object-name"
-
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(bucket_name)
-    blob = bucket.blob(destination_blob_name)
-
-    buf = io.BytesIO()
-
-    plt.imshow(image)
-    plt.axis('off')
-    plt.savefig(buf, format='png')
-
-    blob.upload_from_string(
-        buf.getvalue(),
-        content_type='image/png')
-
-    buf.close()
